e3e87c7f666d45dd84b7b6e8cd40c41f1607b9ab
Sam Zilli <samzilli01@gmail.com>
2024-07-08 16:22:30 -0400

    added name, github link, section

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
   return (
     <div className="App">
       <div>
+        Sam Zilli - Summer 2 2024 - CS5610.61065.202460 
+        https://github.com/Sam-Zilli/kanbas-react-web-app
         <Labs />
       </div>
 

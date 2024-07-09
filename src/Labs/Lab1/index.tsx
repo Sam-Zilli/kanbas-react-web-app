f8808e43abd2d4df04dba2716aeb4dfa87c86f94
Sam Zilli <samzilli01@gmail.com>
2024-07-09 00:05:17 -0400

    link to landing page from Kanbas

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -8,8 +8,9 @@ export default function Lab1() {
         </p>
         <p id="wd-p-4">
           Summer 2 2024: CS5610.61065.202460
+        </p>
+        <p id="wd-p-5">
           <a href="https://github.com/Sam-Zilli/kanbas-react-web-app">Link to Kanbas React Web App GitHub Repository</a>
-
         </p>
       </div>
       </>

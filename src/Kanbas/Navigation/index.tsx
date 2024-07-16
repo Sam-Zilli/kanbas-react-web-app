7aa72fac8c7288a1853fc4d3c6d0be387469c340
Sam Zilli <samzilli01@gmail.com>
2024-07-16 15:50:41 -0400

    ran run build

--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -14,7 +14,7 @@ export default function KanbasNavigation() {
   return (
     <div id="wd-kanbas-navigation" style={{ width: 110 }}
          className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
-      <a id="wd-neu-link" target="_blank" 
+      <a id="wd-neu-link" rel="noreferrer" target="_blank" 
         href="https://www.northeastern.edu/"
         className="list-group-item">
         <img src="/images/NEU.png" width="75px" /> </a>
@@ -58,21 +58,3 @@ export default function KanbasNavigation() {
     </div>
 
 );}
-
-
-
-
-// ASSIGNMENT 1:
-// export default function KanbasNavigation() {
-//     return (
-//       <ul id="wd-kanbas-navigation">
-//         <li><a id="wd-neu-link" href="https://www.northeastern.edu/">Northeastern</a></li>
-//         <li><a id="wd-account-link" href="#/Kanbas/Account">Account</a></li>
-//         <li><a id="wd-dashboard-link" href="#/Kanbas/Dashboard">Dashboard</a></li>
-//         <li><a id="wd-course-link" href="#/Kanbas/Courses">Courses</a></li>
-//         <li><a id="wd-calendar-link" href="#/Kanbas/Calendar">Calendar</a></li>
-//         <li><a id="wd-inbox-link" href="#/Kanbas/Inbox">Inbox</a></li>
-//         <li><a id="wd-labs-link" href="#/Labs">Labs</a></li>
-//         {/* <li><a id="wd-landingPage-link" href="/">Landing Page</a></li> */}
-//       </ul>
-//   );}

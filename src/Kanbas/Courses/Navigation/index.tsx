a3358a643a9c0b12007706c75b22d1ff948b9e7a
Sam Zilli <samzilli01@gmail.com>
2024-07-19 08:53:01 -0400

    removed conversion of links to lowerCase

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router";
+import { useParams, useLocation } from "react-router";
 import { courses } from "../../Database";
 
 import "./index.css"
@@ -6,13 +6,18 @@ import "./index.css"
 export default function CoursesNavigation() {
   const { cid } = useParams();
   const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
   
   const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades"];
 
   return (
     <div id="wd-courses-navigation" className="list-group fs-5 rounded-0">
       {links.map((link, index) => (
-        <a key={index} href={`#/Kanbas/Courses/${course?._id}/${link.toLowerCase()}`}>
+        <a
+          key={index}
+          href={`#/Kanbas/Courses/${cid}/${link}`}
+          className={pathname.includes(link.toLowerCase()) ? 'active' : ''}
+        >
           {link}
         </a>
       ))}

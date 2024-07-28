08c983508ec2839a316ff107984b237a6effad92
Sam Zilli <samzilli01@gmail.com>
2024-07-28 07:42:14 -0400

    before refactoring assingments index.tsx

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -6,22 +6,21 @@ import * as db from "../../Database";
 import { PiDotsSixVerticalFill } from "react-icons/pi";
 import { LuNewspaper } from "react-icons/lu";
 import "./index.css";
+import { useState } from "react";
+import { addAssignment, editAssignment, updateAssignment, deleteAssignment } from "./reducer";
+import { useSelector, useDispatch } from "react-redux";
 
 export default function Assignments() {
   const { cid } = useParams();
-  const assignments = db.assignments;
-
-  const chosenAssignments = assignments.filter(assignment => assignment.course === cid);
-
+  const [assignmentName, setAssignmentName] = useState("");
+  const assignments = useSelector((state: any) => state.assignmentsReducer.assignments);
+  const dispatch = useDispatch();
 
   return (
     <div id="wd-assignments" className="container mt-4">
-
       {/* Making all the buttons be in one row */}
       <div className="d-flex justify-content-between align-items-center mb-4">
-
-
-      {/* Search */}
+        {/* Search */}
         <div className="position-relative w-50">
           <input
             id="wd-search-assignment"
@@ -31,24 +30,18 @@ export default function Assignments() {
           <FaMagnifyingGlass className="position-absolute top-50 start-0 translate-middle-y ms-2 text-muted" />
         </div>
 
-
-      {/* Group */}
+        {/* Group */}
         <div>
           <button id="wd-add-assignment-group" className="btn btn-success me-2">
             + Group
           </button>
 
-
-
-        {/* Assignment */}
+          {/* Assignment */}
           <button id="wd-add-assignment" className="btn btn-primary">
             + Assignment
           </button>
         </div>
       </div>
-
-
-
 
       {/* Assignmnets Header with Oval-shaped Pill thing */}
       <div className="card mb-4">
@@ -65,10 +58,9 @@ export default function Assignments() {
         </div>
       </div>
 
-
       {/* Assignment List */}
       <ul id="wd-assignment-list" className="list-group">
-        {chosenAssignments.map((assignment) => (
+        {assignments.map((assignment) => (
           <li
             key={assignment._id}
             className="wd-assignment-list-item list-group-item p-3 mb-3 border border-secondary rounded-3 bg-white assignment-card"
@@ -79,7 +71,7 @@ export default function Assignments() {
               <div className="d-flex flex-grow-1 align-items-center me-3">
                 <BsGripVertical className="me-3 fs-4" />
                 <div className="flex-grow-1">
-                <Link
+                  <Link
                     to={`/Kanbas/Courses/${cid}/Assignments/${assignment._id}`}
                     className="wd-assignment-link text-dark text-decoration-none d-block mb-2"
                   >
@@ -89,7 +81,7 @@ export default function Assignments() {
                     <span className="text-danger">Multiple Modules</span> |{" "}
                     {/* <strong>Not available until</strong>{" "}
                     {assignment.availableOn} at {assignment.timeDue} | */}
-                    <strong>Not available until</strong>{" "} 12/1/2024 at 00:00AM |
+                    <strong>Not available until</strong> 12/1/2024 at 00:00AM |
                     <br />
                     {/* <strong>Due</strong> {assignment.dueDate} at{" "}
                     {assignment.timeDue} | {assignment.points} pts */}
@@ -103,7 +95,6 @@ export default function Assignments() {
           </li>
         ))}
       </ul>
-
     </div>
   );
 }
--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import ModulesControls from "./ModulesControls";
 import ModuleControlButtons from "./ModuleControlButtons";
 import { useParams } from "react-router";

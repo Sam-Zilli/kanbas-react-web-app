d66a91714a12bf0395cd3703c6e3d6268c6b5657
Sam Zilli <samzilli01@gmail.com>
2024-07-28 14:44:18 -0400

    added confirm delete assgnment dialog box

--- src/Kanbas/Courses/Assignments/AssignmentControlButtons.tsx
+++ src/Kanbas/Courses/Assignments/AssignmentControlButtons.tsx
@@ -1,18 +1,36 @@
-import { IoEllipsisVertical } from "react-icons/io5";
-import GreenCheckmark from "../Modules/GreenCheckmark";
-import { BsPlus } from "react-icons/bs";
+// import { IoEllipsisVertical } from "react-icons/io5";
+// import GreenCheckmark from "../Modules/GreenCheckmark";
+// import { BsPlus } from "react-icons/bs";
+// import { FaPencil } from "react-icons/fa6";
+
+import React, { useState } from "react";
 import { FaTrash } from "react-icons/fa";
-import { FaPencil } from "react-icons/fa6";
+import ConfirmationDialog from "./ConfirmationDialog";
 
 export default function AssignmentControlButtons({
   assignmentId,
   deleteAssignment,
-  // editAssignment,
-}: {
+}: // editAssignment,
+{
   assignmentId: string;
   deleteAssignment: (assignmentId: string) => void;
   editAssignment: (assignmentId: string) => void;
 }) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleDeleteClick = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleConfirmDelete = () => {
+    deleteAssignment(assignmentId);
+    setIsDialogOpen(false);
+  };
+
+  const handleCancelDelete = () => {
+    setIsDialogOpen(false);
+  };
+
   return (
     <div className="d-flex align-items-center">
       {/* <FaPencil
@@ -21,11 +39,18 @@ export default function AssignmentControlButtons({
       /> */}
       <FaTrash
         className="text-danger me-2 fs-5 cursor-pointer"
-        onClick={() => deleteAssignment(assignmentId)}
+        onClick={handleDeleteClick}
       />
       {/* <GreenCheckmark className="me-2" />
       <BsPlus className="fs-1 me-2" />
       <IoEllipsisVertical className="fs-4" /> */}
+
+      {/* Confirmation Dialog */}
+      <ConfirmationDialog
+        isOpen={isDialogOpen}
+        onConfirm={handleConfirmDelete}
+        onCancel={handleCancelDelete}
+      />
     </div>
   );
 }

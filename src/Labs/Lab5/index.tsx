b26228d149c4a616ccecda99a15ebb9037147274
Sam Zilli <samzilli01@gmail.com>
2024-08-04 13:25:08 -0400

    starting A5 3.2

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,16 +1,21 @@
+import EnvironmentVariables from "./EnvironmentVariables";
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+
 export default function Lab5() {
   return (
     <div id="wd-lab5">
       <h2>Lab 5</h2>
       <div className="list-group">
-        <a
-          href="http://localhost:4000/lab5/welcome"
-          className="list-group-item"
-        >
+        <a href={`${REMOTE_SERVER}/lab5/welcome`} className="list-group-item">
           Welcome
         </a>
-      </div>
-      <hr />
+
+        <a href={`${REMOTE_SERVER}/lab5/add/5/10`} className="list-group-item">
+          Add 5 + 10
+        </a>
+
+      </div><hr />
+      <EnvironmentVariables />
     </div>
   );
 }

50786163a34d10d03bbc7f50ed24b96d6deb6eec
Sam Zilli <samzilli01@gmail.com>
2024-08-05 22:55:15 -0400

    working on deployment

--- kanbas-node-server-app/Hello.js
+++
@@ -1,8 +0,0 @@
-export default function Hello(app) {
-  app.get('/hello', (req, res) => {
-    res.send('Yay it is working')
-  })
-  app.get('/', (req, res) => {
-    res.send('Welcome to Full Stack Development!')
-  })
-}

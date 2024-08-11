a7b6c1c817c2b43745eeca8c5da38ab884e206db
Sam Zilli <samzilli01@gmail.com>
2024-08-11 19:09:39 -0400

    courses working

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import * as client from "./client";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
 
 export default function Signin() {

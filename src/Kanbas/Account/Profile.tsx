7f8bdf8500011cf843b8730fe71ef58376330b7b
Sam Zilli <samzilli01@gmail.com>
2024-08-11 10:16:09 -0400

    Starting 5.5.2

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -1,13 +1,14 @@
 import * as client from "./client";
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+
 export default function Profile() {
+
   const [profile, setProfile] = useState<any>({});
-  const navigate = useNavigate();
   const fetchProfile = async () => {
     const account = await client.profile();
     setProfile(account);
   };
+  
   useEffect(() => { fetchProfile(); }, []);
   return (
     <div className="wd-profile-screen">

ac6078023c22768335484afe45987ced6fd75754
Sam Zilli <samzilli01@gmail.com>
2024-08-13 00:11:40 -0400

    Trying full app again

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./index.css";
-// import App from './App';
+import App from './App';
 
 
 const root = ReactDOM.createRoot(
@@ -11,14 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <div>
-      aslka;sldk
-    </div>
+    <App />
+    {/* <div>
+      Testing
+    </div> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();

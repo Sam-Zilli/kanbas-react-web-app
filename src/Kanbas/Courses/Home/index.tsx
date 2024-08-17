64fa2aad3b995f9b68010f4c830d22a9946184fc
Sam Zilli <samzilli01@gmail.com>
2024-08-17 15:51:42 -0400

    module stuff appearing

--- src/Kanbas/Courses/Home/index.tsx
+++ src/Kanbas/Courses/Home/index.tsx
@@ -1,38 +1,18 @@
-// import Modules from "../Modules";
-// import CourseStatus from "./Status";
-// // import 'bootstrap/dist/css/bootstrap.min.css';
+import Modules from "../Modules";
+import CourseStatus from "./Status";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-// export default function Home() {
-//   {console.log("IN HGOMEE!!")}
-//   return (
-//     <table id="wd-home">
-//       asdljkals;djka;lskd;alskd;alskd;laskd;aksd;lkas;dlkas;dka;sldk;asdk;alskd;laskd;ask
-//       <tr>
-//         <td valign="top">
-//           <Modules />
-//         </td>
-//         <td valign="top">
-//           <CourseStatus />
-//         </td>
-//       </tr>
-//     </table>
-//   );
-// }
-
-
-import React from 'react';
-import { useParams } from 'react-router-dom';
-
-const Home = () => {
-  const { courseId } = useParams();
-
+export default function Home() {
   return (
-    <div>
-      <h1>Course Home</h1>
-      <p>Course ID: {courseId}</p>
-      {/* Fetch and display course details based on courseId */}
-    </div>
+    <table id="wd-home">
+      <tr>
+        <td valign="top">
+          <Modules />
+        </td>
+        <td valign="top">
+          <CourseStatus />
+        </td>
+      </tr>
+    </table>
   );
-};
-
-export default Home;
+}
